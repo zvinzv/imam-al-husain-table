8c0186f991715a9c81083be6dc7dd7de7958132a
ZVINZV | DEV <zain.business.co@gmail.com>
2023-11-01 23:12:09 +0300

    Version 2.2.3.

--- src/func/GetDateFromTelegram.ts
+++ src/func/GetDateFromTelegram.ts
@@ -1,5 +1,7 @@
+import { headers } from "next/headers"
+
 export default async function GetDateFromTelegram(offset?:string){
-  const response = await fetch(offset ? `api/getDateFromTelegram?offset=${offset}` : `api/getDateFromTelegram`, {
+  const response = await fetch(offset ? `${headers().get("url")}/api/getDateFromTelegram?offset=${offset}` : `${headers().get("url")}/api/getDateFromTelegram`, {
     cache:"no-store",
   })
   return response.json()
