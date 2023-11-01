ce4ba75d31236727c290e3f5d63f0c65b4512670
ZVINZV | DEV <zain.business.co@gmail.com>
2023-11-01 22:59:12 +0300

    version 2.2.1.

--- src/func/GetDateFromTelegram.ts
+++ src/func/GetDateFromTelegram.ts
@@ -1,5 +1,5 @@
 export default async function GetDateFromTelegram(offset?:string){
-  const response = await fetch(offset ? `http://localhost:3000/api/getDateFromTelegram?offset=${offset}` : `http://localhost:3000/api/getDateFromTelegram`, {
+  const response = await fetch(offset ? `https://imam-al-husain-table.vercel.app/api/getDateFromTelegram?offset=${offset}` : `https://imam-al-husain-table.vercel.app/api/getDateFromTelegram`, {
     cache:"no-store",
   })
   return response.json()
