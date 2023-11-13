0cea2afa1d7d6593ebf84f345ab8872f78ff195a
ZVINZV | DEV <101864805+zvinzv@users.noreply.github.com>
2023-11-13 21:25:50 +0300

    Version 5.2.0, Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { zincirect } from "next/navigation"
+import { redirect } from "next/navigation"
 import { Metadata } from "next"
 export const metadata: Metadata = {
   title: 'root.',
   description: 'هذه الصفحة تحتوي على آخر الاخبار على الموقع.',
 }
 export default async function Home() {
-  zincirect("/home-work")
+  redirect("/home-work")
   return (
     <>
     </>
